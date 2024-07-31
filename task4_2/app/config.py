1b2aa4083466f0f17af9c9638922ed5c2b28beb8
224193867 <tharidhip172002@gmail.com>
2024-07-31 20:45:30 +1000

    modified DB URL

--- task4_2/app/config.py
+++ task4_2/app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("postgresql://sit722_week4_user:SW9n0L9IJKwk3IlLrq3pKGRuF3KbTpOp@dpg-cql18il6l47c73f17g90-a.oregon-postgres.render.com/sit722_week4")
 
 settings = Settings()
